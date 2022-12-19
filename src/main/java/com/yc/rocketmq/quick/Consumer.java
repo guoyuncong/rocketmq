21164fe08dcba40f7dc78ba40f11abb5876e00be
guoyuncong <guoyuncong@lazbao.com>
2022-12-19 18:14:58 +0800

    feat：quick start

--- src/main/java/com/yc/rocketmq/quick/Consumer.java
+++ src/main/java/com/yc/rocketmq/quick/Consumer.java
@@ -1,64 +1,48 @@
 package com.yc.rocketmq.quick;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
 
 /**
  * This example shows how to subscribe and consume messages using providing {@link DefaultMQPushConsumer}.
  */
 public class Consumer {
-
-    public static final String CONSUMER_GROUP = "please_rename_unique_group_name_4";
-    public static final String DEFAULT_NAMESRVADDR = "101.35.111.41:9876";
-    public static final String TOPIC = "TopicTest";
-
     public static void main(String[] args) throws InterruptedException, MQClientException {
-
         /*
          * Instantiate with specified consumer group name.
          */
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
-
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_4");
         /*
-         * Specify name server addresses.
-         * <p/>
-         *
-         * Alternatively, you may specify name server addresses via exporting environmental variable: NAMESRV_ADDR
-         * <pre>
-         * {@code
-         * consumer.setNamesrvAddr("name-server1-ip:9876;name-server2-ip:9876");
-         * }
-         * </pre>
+         * Specify where to start in case the specified consumer group is a brand new one.
          */
-        // Uncomment the following line while debugging, namesrvAddr should be set to your local address
-        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
-
-        /*
-         * Specify where to start in case the specific consumer group is a brand-new one.
-         */
+        consumer.setNamesrvAddr("101.35.111.41:9876");
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-
         /*
-         * Subscribe one more topic to consume.
+         * Subscribe one more more topics to consume.
          */
-        consumer.subscribe(TOPIC, "*");
-
+        consumer.subscribe("TopicTest", "*");
         /*
          *  Register callback to execute on arrival of messages fetched from brokers.
          */
-        consumer.registerMessageListener((MessageListenerConcurrently) (msg, context) -> {
-            System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msg);
-            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
         });
-
         /*
          *  Launch the consumer instance.
          */
         consumer.start();
-
         System.out.printf("Consumer Started.%n");
     }
 }
